6b5ffffb00db8f045adefa0c00d43ef6a7dc955a
goldaanto <102153472+goldaanto@users.noreply.github.com>
2024-10-24 08:51:28 -0400

    First Commit

---
+++ DsAlgoProject/src/test/java/stepdef/DsAlgoRunner.java
@@ -0,0 +1,16 @@
+package stepdef;
+
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= "src/test/resources/Features", glue={"stepdef"}, monochrome = true, plugin= {"pretty","html:target/JSONReport/report.html"})
+//dsalog Runner
+public class DsAlgoRunner {
+	
+	
+
+}
