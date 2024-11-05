f5705da2b0a582da61637b1b3d6fee73bac5df82
goldaanto <102153472+goldaanto@users.noreply.github.com>
2024-11-05 10:25:08 -0500

    Updated code

--- DsAlgoProject/src/test/java/stepdef/DsAlgoRunner.java
+++ DsAlgoProject/src/test/java/stepdef/DsAlgoRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= "src/test/resources/Features", glue={"stepdef"}, monochrome = true, plugin= {"pretty","html:target/JSONReport/report.html"})
+@CucumberOptions(features= "src/test/resources/Features", glue={"stepdef","hook"}, monochrome = true, plugin= {"pretty","html:target/JSONReport/report.html"})
 //dsalog Runner
 public class DsAlgoRunner {
 	
